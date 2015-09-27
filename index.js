a710010172d569cc6c9c576261a269eeafff3288
simov <simeonvelichkov@gmail.com>
2015-09-27 18:07:49 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var request = require('request-core')
+
+
+function client (options) {
+  if (options.end === undefined) {
+    options.end = true
+  }
+  return request(options)
+}
+
+module.exports = client
